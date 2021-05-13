3051ee62f5b7d04a401490170704661330993105
Ramazan-Macbook <txmiwxxd@gmail.com>
2021-05-13 19:46:13 +0300

    redone strncmp(with pointers instead of indexes)

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -2,12 +2,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
-	i = 0;
 	if (n == 0)
 		return (0);
-	while (--n && s1[i] == s2[i] && s1[i] && s2[i])
-		i++;
-	return (((unsigned char)s1[i] - (unsigned char)s2[i]));
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (*str1 && *str2 && --n && *(str1) == *(str2))
+	{
+		str1++;
+		str2++;
+	}
+	return (*(str1) - *(str2));
 }
