d5f8776628862384a43d7ac9b9fc1fa998e4f710
Ramazan-Macbook <txmiwxxd@gmail.com>
2021-05-15 23:19:46 +0300

    fix malloc in strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -11,7 +11,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	str2 = (char *)s2;
 	if (!s1 || !s2)
 		return (NULL);
-	j_str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)));
+	j_str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!j_str)
 		return (NULL);	
 	start_str = j_str;
