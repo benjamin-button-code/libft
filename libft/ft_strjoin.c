7239c48d81e86f62a57188f7613eaf7c803c7d46
Ramazan-Macbook <txmiwxxd@gmail.com>
2021-05-15 11:49:34 +0300

    change strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -3,21 +3,31 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*j_str;
-	size_t	i;
-	size_t	j;
+	char	*start_str;
+	char	*str1;
+	char	*str2;
 
-	i = 0;
-	j = 0;
+	str1 = (char *)s1;
+	str2 = (char *)s2;
 	if (!s1 || !s2)
 		return (NULL);
-	j_str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	j_str = (char *)malloc(sizeof(char) * (ft_strlen(s1)
+				+ ft_strlen(s2) + 1));
 	if (!j_str)
-		return (NULL);
-	while (s1[i])
-		j_str[j++] = s1[i++];
-	i = 0;
-	while (s2[i])
-		j_str[j++] = s2[i++];
-	j_str[j] = '\0';
-	return (j_str);
+		return (NULL);	
+	start_str = j_str;
+	while (*str1)
+	{
+		*j_str = *str1;
+		str1++;
+		j_str++;
+	}
+	while (*str2)
+	{
+		*j_str = *str2;
+		str2++;
+		j_str++;
+	}
+	*j_str = '\0';
+	return (start_str);
 }
